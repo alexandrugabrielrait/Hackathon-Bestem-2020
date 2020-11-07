c493f430acdd81e6d8819484e54f4d723473a72d
constantin.mihailov <constantin.mihailov@stud.acs.upb.ro>
2020-11-07 19:09:19 +0200

    modify file read

---
+++ src/file_names.py
@@ -0,0 +1,7 @@
+
+INPUT = "../input.txt"
+CLIENT_SYN = "../keywords/client_synonyms.txt"
+COMPANY_SYN = "../keywords/company_synonyms.txt"
+PERMISSIVE_VERBS = "../keywords/permissive_verbs.txt"
+OBLIGATORY_VERBS = "../keywords/obligatory_verbs.txt"
+CONST_OBLIGATORY_VERBS = "../keywords/obligatory_verbs_constant.txt"
